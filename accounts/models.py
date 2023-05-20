461fdbd61bfc98340d105735c61134dbf10d3f0c
hardrayscode <dareadebayo42@gmail.com>
2023-05-20 15:31:26 -0700

    intial commit

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from accounts.managers import UserManager
+
+
+class User(AbstractUser):
+    
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    objects = UserManager()
